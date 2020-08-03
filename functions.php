6972bc6f000779d87e7bda9b2ecc0d5d25e5af56
Trouni Tiet <trouni@gmail.com>
2020-08-03 23:36:18 +0900

    beautify

--- functions.php
+++ functions.php
@@ -13,17 +13,17 @@
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
 function enqueue_custom_styles()
 {
-    foreach (glob(get_stylesheet_directory() . '/css/*.css') as $file) {
-        // $file returns the absolute path
-        $filename = substr($file, strrpos($file, '/') + 1);
-        wp_enqueue_style($file, get_stylesheet_directory_uri() . '/css/' . $filename);
-    }
+  foreach (glob(get_stylesheet_directory() . '/css/*.css') as $file) {
+    // $file returns the absolute path
+    $filename = substr($file, strrpos($file, '/') + 1);
+    wp_enqueue_style($file, get_stylesheet_directory_uri() . '/css/' . $filename);
+  }
 
-    foreach (glob(get_stylesheet_directory() . '/css/vendors/*.css') as $file) {
-        // $file returns the absolute path
-        $filename = substr($file, strrpos($file, '/') + 1);
-        wp_enqueue_style($file, get_stylesheet_directory_uri() . '/css/vendors/' . $filename);
-    }
+  foreach (glob(get_stylesheet_directory() . '/css/vendors/*.css') as $file) {
+    // $file returns the absolute path
+    $filename = substr($file, strrpos($file, '/') + 1);
+    wp_enqueue_style($file, get_stylesheet_directory_uri() . '/css/vendors/' . $filename);
+  }
 }
 
 /**
@@ -32,11 +32,11 @@ function enqueue_custom_styles()
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 function enqueue_custom_scripts()
 {
-    foreach (glob(get_stylesheet_directory() . '/js/*.js') as $file) {
-        // $file returns the absolute path
-        $filename = substr($file, strrpos($file, '/') + 1);
-        wp_enqueue_script($file, get_stylesheet_directory_uri() . '/js/' . $filename);
-    }
+  foreach (glob(get_stylesheet_directory() . '/js/*.js') as $file) {
+    // $file returns the absolute path
+    $filename = substr($file, strrpos($file, '/') + 1);
+    wp_enqueue_script($file, get_stylesheet_directory_uri() . '/js/' . $filename);
+  }
 }
 
 /**
