ac510eaef4be630d1439472e269855a76403cd9a
Trouni Tiet <trouni@gmail.com>
2020-08-07 00:33:07 +0900

    updated responsiveness

--- inc/scss-variables.php
+++ inc/scss-variables.php
@@ -5,7 +5,7 @@ function wp_scss_set_variables()
 {
   $variables = array(
     'nav-padding' => '3.5rem',
-    'main-padding' => '14rem',
+    'main-padding' => '3.5rem',
     'arrow-img' => 'url(/wp-content/uploads/2020/08/arrow_down.png)'
   );
   return $variables;
