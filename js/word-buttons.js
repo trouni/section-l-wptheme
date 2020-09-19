ca5c8ece543f4707977b38cffef7233e6dbcc599
Trouni Tiet <trouni@gmail.com>
2020-09-19 21:43:44 +0900

    word photos component finished

--- js/word-buttons.js
+++ js/word-buttons.js
@@ -1,13 +1,26 @@
 const wordPhotos = document.querySelectorAll('[data-word]')
 const wordButtons = document.querySelectorAll('.round-word-btn')
+let activeButton = 0
+
+const activateButton = (button) => {
+  wordPhotos.forEach(photo => photo.classList.remove('active'))
+  wordButtons.forEach(photo => photo.classList.remove('active'))
+  const word = button.innerText.toLowerCase()
+  button.classList.add('active')
+  document.querySelector(`[data-word=${word}]`).classList.add('active')
+}
+
+const cyclePhotos = () => {
+  activeButton = (activeButton + 1) % wordButtons.length
+  activateButton(wordButtons[activeButton])
+}
+
+const cycleInterval = setInterval(cyclePhotos, 3000)
 
 wordButtons.forEach(button => {
   button.addEventListener('click', e => {
     e.preventDefault()
-    const word = button.innerText.toLowerCase()
-    wordPhotos.forEach(photo => photo.classList.remove('active'))
-    wordButtons.forEach(photo => photo.classList.remove('active'))
-    button.classList.add('active')
-    document.querySelector(`[data-word=${word}]`).classList.add('active')
+    clearInterval(cycleInterval)
+    activateButton(button)
   })
 })
