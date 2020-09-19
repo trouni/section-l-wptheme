22dea4a9c80705863a976a01cbe7205a63be88ff
Trouni Tiet <trouni@gmail.com>
2020-09-19 23:49:22 +0900

    added animation for round buttons

--- js/word-buttons.js
+++ js/word-buttons.js
@@ -1,13 +1,15 @@
+const activeClasses = 'active animate__animated animate__faster animate__pulse'
+
 const wordPhotos = document.querySelectorAll('[data-word]')
 const wordButtons = document.querySelectorAll('.round-word-btn')
 let activeButton = 0
 
 const activateButton = (button) => {
-  wordPhotos.forEach(photo => photo.classList.remove('active'))
-  wordButtons.forEach(photo => photo.classList.remove('active'))
+  wordPhotos.forEach(photo => photo.classList.remove(activeClasses))
+  wordButtons.forEach(photo => photo.classList.remove(activeClasses))
   const word = button.innerText.toLowerCase()
-  button.classList.add('active')
-  document.querySelector(`[data-word=${word}]`).classList.add('active')
+  button.classList.add(activeClasses)
+  document.querySelector(`[data-word=${word}]`).classList.add(activeClasses)
 }
 
 const cyclePhotos = () => {
