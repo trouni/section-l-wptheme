2cb36905628fd627716a7a97989c1ecc66aa4327
Trouni Tiet <trouni@gmail.com>
2020-09-29 01:30:50 +0900

    added div using javascript

---
+++ js/slider-structure.js
@@ -0,0 +1,4 @@
+document.querySelectorAll('.section-l-slider figure.swiper-slide-inner > img.swiper-slide-image:not(:only-child)').forEach(img => {
+  new_html = "<div>" + img.outerHTML + "</div>";
+  img.outerHTML = new_html;
+});
