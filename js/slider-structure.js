7884bb25452077634434e3aa0ca85a7aca370172
Trouni Tiet <trouni@gmail.com>
2020-09-30 16:18:30 +0900

    loading javascripts after dom loaded

--- js/slider-structure.js
+++ js/slider-structure.js
@@ -1,4 +1,6 @@
-document.querySelectorAll('.section-l-slider figure.swiper-slide-inner > img.swiper-slide-image:not(:only-child)').forEach(img => {
-  new_html = "<div>" + img.outerHTML + "</div>";
-  img.outerHTML = new_html;
-});
+document.addEventListener('DOMContentLoaded', _ => {
+  document.querySelectorAll('.section-l-slider figure.swiper-slide-inner > img.swiper-slide-image:not(:only-child)').forEach(img => {
+    new_html = "<div>" + img.outerHTML + "</div>";
+    img.outerHTML = new_html;
+  });
+}
--- js/word-buttons.js
+++ js/word-buttons.js
@@ -1,30 +1,32 @@
-const activeBtnClasses = ['active', 'animate__animated', 'animate__faster', 'animate__pulse']
+document.addEventListener('DOMContentLoaded', _ => {
+  const activeBtnClasses = ['active', 'animate__animated', 'animate__faster', 'animate__pulse']
 
-const wordPhotos = document.querySelectorAll('[data-word]')
-const wordButtons = document.querySelectorAll('.round-word-btn')
-let activeButton = 0
+  const wordPhotos = document.querySelectorAll('[data-word]')
+  const wordButtons = document.querySelectorAll('.round-word-btn')
+  let activeButton = 0
 
-const activateButton = (button) => {
-  const word = button.innerText.toLowerCase()
-  wordPhotos.forEach(photo => photo.classList.remove('active'))
-  document.querySelector(`[data-word=${word}]`).classList.add('active')
-  wordButtons.forEach(photo => photo.classList.remove(...activeBtnClasses))
-  button.classList.add(...activeBtnClasses)
-}
+  const activateButton = (button) => {
+    const word = button.innerText.toLowerCase()
+    wordPhotos.forEach(photo => photo.classList.remove('active'))
+    document.querySelector(`[data-word=${word}]`).classList.add('active')
+    wordButtons.forEach(photo => photo.classList.remove(...activeBtnClasses))
+    button.classList.add(...activeBtnClasses)
+  }
 
-const cyclePhotos = () => {
-  activeButton = (activeButton + 1) % wordButtons.length
-  activateButton(wordButtons[activeButton])
-}
+  const cyclePhotos = () => {
+    activeButton = (activeButton + 1) % wordButtons.length
+    activateButton(wordButtons[activeButton])
+  }
 
-if (wordButtons.length > 0) {
-  const cycleInterval = setInterval(cyclePhotos, 3000)
-  
-  wordButtons.forEach(button => {
-    button.addEventListener('click', e => {
-      e.preventDefault()
-      clearInterval(cycleInterval)
-      activateButton(button)
+  if (wordButtons.length > 0) {
+    const cycleInterval = setInterval(cyclePhotos, 3000)
+    
+    wordButtons.forEach(button => {
+      button.addEventListener('click', e => {
+        e.preventDefault()
+        clearInterval(cycleInterval)
+        activateButton(button)
+      })
     })
-  })
+  }
 }
