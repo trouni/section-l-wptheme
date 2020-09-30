896acdd31b6173038d298f364d98e72d09a69f70
Trouni Tiet <trouni@gmail.com>
2020-09-30 14:13:23 +0900

    updated loader for fonts and doc ready

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
+;(function(doc, win, add, remove, loaded, load) {
+  doc.ready = new Promise(function(resolve) {
+    if (doc.readyState === 'complete') {
+      resolve();
+    } else {
+      function onReady() {
+        resolve();
+        doc[remove](loaded, onReady, true);
+        win[remove](load, onReady, true);
+      }
+      doc[add](loaded, onReady, true);
+      win[add](load, onReady, true);
+    }
+  });
+})(document, window, 'addEventListener', 'removeEventListener', 'DOMContentLoaded', 'load');
+
+const waitToLoad = [
+  document.ready,
+  document.fonts.ready
+]
+
+Promise.all(waitToLoad).then(function () {
   const startAnimation = document.querySelector('.start-animation')
   startAnimation.classList.add('animate')
-}, false);
+});
