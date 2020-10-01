7af2ef7eb04a2d9fe49d05b3a666d9c65fdaac8c
Trouni Tiet <trouni@gmail.com>
2020-10-01 13:07:46 +0900

    removed dom loaded wait

--- js/beds24.js
+++ js/beds24.js
@@ -1,14 +1,13 @@
-document.addEventListener('DOMContentLoaded', _ => {
-  const loc = window.location.toString(),
-    params = loc.split('?')[1],
-    iframe = document.querySelector('#beds24 iframe');
 
-  const isBeds24Page = loc.match(/section-l\.co\/book/)
+const loc = window.location.toString(),
+  params = loc.split('?')[1],
+  iframe = document.querySelector('#beds24 iframe');
 
-  if (isBeds24Page) {
-    iframe.src = iframe.src + '?' + params;
-    // Can't access iframe on other domain, so code below doesn't work :(
-    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-    iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + '<link rel="stylesheet" href="https://www.section-l.co/wp-content/themes/section-l-wptheme/css/beds24.css">';
-  }
-})
+const isBeds24Page = loc.match(/section-l\.co\/book/)
+
+if (isBeds24Page) {
+  iframe.src = iframe.src + '?' + params;
+  // Can't access iframe on other domain, so code below doesn't work :(
+  iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+  iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + '<link rel="stylesheet" href="https://www.section-l.co/wp-content/themes/section-l-wptheme/css/beds24.css">';
+}
