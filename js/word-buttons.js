38a0fd4cb221f50eaf6c8b46fb67a1a2499f331c
Trouni Tiet <trouni@gmail.com>
2020-10-06 19:11:22 +0900

    word-buttons using indexes

--- js/word-buttons.js
+++ js/word-buttons.js
@@ -1,32 +1,29 @@
 document.addEventListener('DOMContentLoaded', _ => {
   const activeBtnClasses = ['active', 'animate__animated', 'animate__faster', 'animate__pulse']
-  const wordPhotos = document.querySelectorAll('[data-word]')
+  const wordPhotos = document.querySelectorAll('.word-photo')
   const wordButtons = document.querySelectorAll('.round-word-btn')
   let activeBtnIndex = 0
 
-  const activateButton = (button) => {
-    const word = button.innerText.trim().toLowerCase()
-    if (word !== "") {
-      wordPhotos.forEach(photo => photo.classList.remove('active'))
-      document.querySelector(`[data-word=${word}]`).classList.add('active')
-      wordButtons.forEach(photo => photo.classList.remove(...activeBtnClasses))
-      button.classList.add(...activeBtnClasses)
-    }
+  const activateButton = (index) => {
+    wordPhotos.forEach(photo => photo.classList.remove('active'))
+    wordPhotos[index].classList.add('active')
+    wordButtons.forEach(wordButton => wordButton.classList.remove(...activeBtnClasses))
+    wordButtons[index].classList.add(...activeBtnClasses)
   }
 
   const cyclePhotos = () => {
     activeBtnIndex = (activeBtnIndex + 1) % wordButtons.length
-    activateButton(wordButtons[activeBtnIndex])
+    activateButton(activeBtnIndex)
   }
 
   if (wordButtons.length > 0) {
     const cycleInterval = setInterval(cyclePhotos, 3000)
     
-    wordButtons.forEach(button => {
+    wordButtons.forEach((button, index) => {
       button.addEventListener('click', e => {
         e.preventDefault()
         clearInterval(cycleInterval)
-        activateButton(button)
+        activateButton(index)
       })
     })
   }
