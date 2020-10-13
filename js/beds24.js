a7593de1eb585e833e96dd376420743714e8f650
Trouni Tiet <trouni@gmail.com>
2020-10-13 21:15:43 +0900

    fixed order of doc load

--- js/beds24.js
+++ js/beds24.js
@@ -2,15 +2,15 @@
 const loc = window.location.toString(),
   params = loc.split('?')[1];
 
-const beds24Page = document.getElementById('beds24');
-
 document.addEventListener('DOMContentLoaded', _ => {
-  if (beds24Page) {  
+  const beds24Page = document.getElementById('beds24');
+
+  if (beds24Page) {
     iframe = document.querySelector('#beds24 iframe');
     iframe.src = iframe.src + '?' + params;
     // // Can't access iframe on other domain, so code below doesn't work :(
     //   iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
     //   iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + '<link rel="stylesheet" href="https://www.section-l.co/wp-content/themes/section-l-wptheme/css/beds24.css">';
-    document.getElementById('beds24').classList.remove('invisible')
+    beds24Page.classList.remove('invisible')
   }
 })
