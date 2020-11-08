4cbacf3f8e3ec08b1c4892a7a6d321259d4ed678
Trouni Tiet <trouni@gmail.com>
2020-11-08 12:48:49 +0900

    fixed query params for iframe

--- js/beds24.js
+++ js/beds24.js
@@ -1,16 +1,13 @@
-
-const loc = window.location.toString(),
-  params = loc.split('?')[1];
+const urlParams = new URLSearchParams(window.location.search);
 
 document.addEventListener('DOMContentLoaded', _ => {
   const beds24Page = document.getElementById('beds24');
 
   if (beds24Page) {
     iframe = document.querySelector('#beds24 iframe');
-    iframe.src = iframe.src + '?' + params;
-    // // Can't access iframe on other domain, so code below doesn't work :(
-    //   iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-    //   iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + '<link rel="stylesheet" href="https://www.section-l.co/wp-content/themes/section-l-wptheme/css/beds24.css">';
+    iframeParams = new URLSearchParams(iframe.src.split('?')[1])
+    urlParams.keys().forEach(key=>iframeParams.append(key, urlParams.get(key)))
+    iframe.src = iframe.src.split('?')[0] + '?' + iframeParams;
     beds24Page.classList.remove('invisible')
   }
 })
